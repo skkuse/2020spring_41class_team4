00add13aed5451f97b52ac4700bce8cb5bc12361
BearHunter49 <rkdcjf987@naver.com>
2020-05-24 20:43:57 +0900

    Set rest_framework viewset

--- src/Backend/rest/views.py
+++ src/Backend/rest/views.py
@@ -1,3 +1,57 @@
 from django.shortcuts import render
+from rest_framework import viewsets, views
+from .serializers import *
+from .models import *
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    If you want change each REST API personally,
+    def list(self, request):
+            pass
+
+        def create(self, request):
+            pass
+
+        def retrieve(self, request, pk=None):
+            pass
+
+        def update(self, request, pk=None):
+            pass
+
+        def partial_update(self, request, pk=None):
+            pass
+
+        def destroy(self, request, pk=None):
+            pass
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class ScheduleViewSet(viewsets.ModelViewSet):
+    queryset = Schedule.objects.all()
+    serializer_class = ScheduleSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class LockerViewSet(viewsets.ModelViewSet):
+    queryset = Locker.objects.all()
+    serializer_class = LockerSerializer
+
+
