f2af8f5ced992b7207660233ef1dffec7583f441
BearHunter49 <rkdcjf987@naver.com>
2020-05-24 20:24:34 +0900

    admin 등록

--- src/Backend/rest/admin.py
+++ src/Backend/rest/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Product)
+admin.site.register(Post)
+admin.site.register(Schedule)
+admin.site.register(Course)
+admin.site.register(Locker)
+
